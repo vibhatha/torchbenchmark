81aa7f4a8226f77089dfa6bd7a55a7e4ef034d2f
vibhatha <vibhatha@gmail.com>
2020-03-10 19:16:29 -0400

    adding alexnet and change initi code

--- copy_benchmark/alexnet_copy.py
+++ copy_benchmark/alexnet_copy.py
@@ -4,47 +4,7 @@ import torch.optim as optim
 import numpy as np
 import timeit
 from estimator.pytorch_modelsize import SizeEstimator
-
-
-class AlexNet(nn.Module):
-
-    def __init__(self, num_classes=1000):
-        super(AlexNet, self).__init__()
-
-        self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
-
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
-
-    def forward(self, x):
-        x = self.features(x)
-        x = self.avgpool(x)
-        x = torch.flatten(x, 1)
-        x = self.classifier(x)
-        return x
+from network.AlexNet import AlexNet
 
 
 num_classes = 1000
@@ -61,7 +21,7 @@ print("Cuda Available : {}".format(cuda_available))
 print("===================================================")
 
 
-def train(model):
+def train(model, epochs=10):
     model.train(True)
     loss_fn = nn.MSELoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001)
@@ -70,24 +30,25 @@ def train(model):
         .random_(0, num_classes) \
         .view(batch_size, 1)
 
-    for _ in range(num_batches):
-        # generate random inputs and labels
-        inputs = torch.randn(batch_size, 3, image_w, image_h)
-        labels = torch.zeros(batch_size, num_classes) \
-            .scatter_(1, one_hot_indices, 1)
+    for epoch in range(epochs):
+        for _ in range(num_batches):
+            # generate random inputs and labels
+            inputs = torch.randn(batch_size, 3, image_w, image_h)
+            labels = torch.zeros(batch_size, num_classes) \
+                .scatter_(1, one_hot_indices, 1)
 
-        # run forward pass
-        optimizer.zero_grad()
-        if cuda_available:
-            outputs = model(inputs.to('cuda:0'))
-        else:
-            outputs = model(inputs)
-        # print("Output-device {}".format(outputs.device))
+            # run forward pass
+            optimizer.zero_grad()
+            if cuda_available:
+                outputs = model(inputs.to('cuda:0'))
+            else:
+                outputs = model(inputs)
+            # print("Output-device {}".format(outputs.device))
 
-        # run backward pass
-        labels = labels.to(outputs.device)
-        loss_fn(outputs, labels).backward()
-        optimizer.step()
+            # run backward pass
+            labels = labels.to(outputs.device)
+            loss_fn(outputs, labels).backward()
+            optimizer.step()
 
 
 import time
