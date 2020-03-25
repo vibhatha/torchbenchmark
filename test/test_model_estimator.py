4509941a972de85a4dafd37225c2d14382703a92
vibhatha <vibhatha@gmail.com>
2020-03-25 10:30:32 -0400

    adding simple test case

--- test/test_model_estimator.py
+++ test/test_model_estimator.py
@@ -1,11 +1,9 @@
 import torch
 from torchvision import models
-from torchbenchmark.network.core import AlexNet
 
 network_name = "resnet152"
 
 num_classes = 1000
-model_alexnet = AlexNet(num_classes=num_classes)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 vgg = models.alexnet().to(device)
